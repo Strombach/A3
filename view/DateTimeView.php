a9a1d2a0123c16b2f2151c6a75b696d58eed2f6b
Fredrik <fredde.stromback@gmail.com>
2019-10-13 21:45:53 +0200

    Enables dateview

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,12 +1,20 @@
 <?php
 
-class DateTimeView {
+date_default_timezone_set('Europe/Stockholm');
+
+class DateTimeView
+{
 
 
-	public function show() {
+  public function show()
+  {
+    $dayText = date('l');
+    $dateNum = date('jS');
+    $monthAndYear = date('F Y');
+    $time = date('G:i:s');
 
-		$timeString = 'TODO, Write servertime here...';
+    $timeString = "$dayText, the $dateNum of $monthAndYear, The time is $time ";
 
-		return '<p>' . $timeString . '</p>';
-	}
+    return '<p>' . $timeString . '</p>';
+  }
 }
