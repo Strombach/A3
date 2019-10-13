3995872601e4821923164201fae9693e069911af
Fredrik <fredde.stromback@gmail.com>
2019-10-13 21:52:21 +0200

    Adds the UserStorage model

---
+++ model/UserStorage.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace model;
+
+class UserStorage
+{
+
+  private $jsonData;
+  private $phpObj;
+
+  public function loadUsers($jsonFile)
+  {
+    $this->jsonData = file_get_contents($jsonFile, true);
+    $this->phpObj = json_decode($this->jsonData);
+  }
+
+  public function findUserByUsername($uname)
+  {
+    for ($i = 0; $i < sizeof($this->phpObj); $i++) {
+      if ($this->phpObj[$i]->username === $uname) {
+        return $this->phpObj[$i];
+      }
+    }
+    return false;
+  }
+
+  public function authAUser($creds)
+  {
+    $user = $this->findUserByUsername($creds[0]);
+
+    if ($user) {
+      if ($user->password == $creds[1]) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  public function saveUser($newUser)
+  { }
+}
